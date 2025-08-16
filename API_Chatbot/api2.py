299f911a75547e16defdd85bc1d4f9889279f01e
Shruti Tiwari <163073836+shruti-tiwari761@users.noreply.github.com>
2025-08-16 21:31:21 +0530

    Update api2.py

--- API_Chatbot/api2.py
+++ API_Chatbot/api2.py
@@ -1,7 +1,7 @@
 from google.generativeai import configure, GenerativeModel
 
 # Configure with your API key
-configure(api_key="AIzaSyAsskJ6QKtwVlTVw3Von1IWMkGuzzCVw4k")
+configure(api_key="")
 
 # Initialize the model
 model = GenerativeModel('gemini-1.5-flash')
@@ -14,3 +14,4 @@ response = model.generate_content(user_question)
 
 # Print the AI's answer
 print("\nGemini says:", response.text)
+
